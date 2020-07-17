e45d1b33c8cfdd1257fbea9a0b5e5e91ae9cb5d0
Fernanda Diomede <fcdiomede@gmail.com>
2020-07-17 14:09:16 -0500

    Added base price functionality and govt melons class

--- melons.py
+++ melons.py
@@ -1,5 +1,7 @@
 """Classes for melon orders."""
 
+from random import randint
+
 class AbstractMelonOrder():
     """ An abstract base class that other melon orders inhert from """
     order_type = "Not Set"
@@ -14,8 +16,12 @@ class AbstractMelonOrder():
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        base_price = get_base_price()
+
+        if self.species == "Christmas":
+            self.base_price *= 1.5
+
+        total = (1 + self.tax) * self.qty * self.base_price
 
         return total
 
@@ -23,6 +29,10 @@ class AbstractMelonOrder():
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
+
+    def get_base_price(self):
+        base_price = randint(5,9)
+
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
@@ -38,10 +48,23 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        super().__init__(self, species, qty)
+        super().__init__(species, qty)
         self.country_code = country_code
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+
+        if self.qty < 10:
+            return super().get_total() + 3.00
+        else:
+            return super().get_total()
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    passed_inspection = False
+
+    def mark_inspection(passed):
+        self.passed_inspection = passed
